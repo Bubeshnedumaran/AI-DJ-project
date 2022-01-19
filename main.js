38993e6e2c3179038f97c09f40c01b17ca16f62c
Bubeshnedumaran <79853076+Bubeshnedumaran@users.noreply.github.com>
2022-01-19 06:54:45 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+song1="";
+song2="";
+leftwristx=0;
+leftwristy=0;
+rightwristx=0;
+rightwristy=0;
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelloaded);
+    poseNet.on('pose',gotposes);
+}
+function draw(){
+    image(video,0,0,600,500);
+}
+function preload(){
+    song1=loadSound("Beliver_tamil.mp3");
+    song2=loadsound("Beliver_English.mp3")
+}
+function modelloaded(){
+    console.log("poseNet is initialized");
+}
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+        leftwristx=results[0].pose.leftWrist.x;
+        leftwristy=results[0].pose.leftWrist.y;
+        console.log("leftwrist x = "+leftwristx+" leftwrist y = "+leftwristy);
+
+        rightwristx=results[0].pose.rightWrist.x;
+        rightwristy=results[0].pose.rightWrist.y;
+        console.log("rightwrist x = "+rightwristx+" rightwrist y = "+rightwristy);
+    }
+}
